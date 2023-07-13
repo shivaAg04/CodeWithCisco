89072a547c66467ccc2d6827cf6acb3f91601f75
Abhi-atg-1021 <abhisinghal112@gmail.com>
2023-07-13 11:45:11 +0530

    change

---
+++ Node.java
@@ -0,0 +1,18 @@
+
+
+public class Node {
+    int isMaster;
+    // String MacAddress;
+    int nodeNO;
+    int subNode;
+    int isSkip;
+
+
+    Node(int n){
+        isMaster=0;
+        nodeNO=n;
+        subNode=0;
+    }
+    
+    
+}
